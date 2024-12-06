102665f3fc51740597db7dbbe2bed0b334f3da98
Carles Serna Uroz <carles-mac@Mac.home>
2024-12-06 18:42:58 +0100

    bar chart added

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="chart">
+    <apexchart
+      type="bar"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+<script setup>
+</script>
