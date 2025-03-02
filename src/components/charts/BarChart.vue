05d4a3d783a4ffb0591da5a4b5a62db9670dfcd1
Carles Serna <carles.serna@gmail.com>
2025-03-02 12:35:01 +0100

    Lógica para anadir barras horizontales

--- src/components/charts/BarChart.vue
+++ src/components/charts/BarChart.vue
@@ -6,12 +6,16 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  horizontal: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const series = ref([
   {
     name: props.data.title || "Valor",
-    data: props.data.data || [], // Usar data directamente, no values
+    data: props.data.data || [],
   },
 ]);
 
@@ -19,6 +23,11 @@ const chartOptions = ref({
   chart: {
     type: "bar",
     height: 350,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: props.horizontal,
+    },
   },
   xaxis: {
     categories: props.data.categories || [],
@@ -36,6 +45,7 @@ watchEffect(() => {
     series.value[0].name = props.data.title;
     chartOptions.value.xaxis.categories = props.data.categories;
     chartOptions.value.yaxis.title.text = props.data.yAxisTitle;
+    chartOptions.value.plotOptions.bar.horizontal = props.horizontal;
   }
 });
 </script>
